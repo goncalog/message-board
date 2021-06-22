14fa0ad2705a071390856f2c6aa1355710d5cae9
Goncalo Gordo <goncalogordo@gmail.com>
2021-06-22 21:08:50 +0100

    fix sql syntax to create tables

--- db/migrations/create_dbs.sql
+++ db/migrations/create_dbs.sql
@@ -1,5 +1,5 @@
 createdb message_board
-CREATE TABLE messages(id SERIAL PRIMARY KEY, name VARCHAR(50), message VARCHAR(200), date DATETIME);
+CREATE TABLE messages(id SERIAL PRIMARY KEY, name VARCHAR(50), message VARCHAR(200), date DATE);
 
 createdb message_board_test
-CREATE TABLE messages(id SERIAL PRIMARY KEY, name VARCHAR(50), message VARCHAR(200), date DATETIME);
+CREATE TABLE messages(id SERIAL PRIMARY KEY, name VARCHAR(50), message VARCHAR(200), date DATE;
