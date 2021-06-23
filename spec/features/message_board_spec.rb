c3c897ad0f277d05acf79d72acd144ee929caa85
Goncalo Gordo <goncalogordo@gmail.com>
2021-06-23 10:57:24 +0100

    add link to messages board in main page

--- spec/features/message_board_spec.rb
+++ spec/features/message_board_spec.rb
@@ -3,4 +3,9 @@ feature 'users can go to main page' do
     visit '/'
     expect(page).to have_content 'Welcome to The Message Board'
   end
+
+  scenario 'and see a link to see all messages' do
+    visit '/'
+    expect(page).to have_link 'See All Messages'
+  end
 end 
